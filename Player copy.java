1d2589c33b2c8f400aa2fd418602269327a7f6c1
nph32 <35321393+nph32@users.noreply.github.com>
2018-01-15 22:34:29 -0500

    Add files via upload

--- Player copy.java
+++ Player copy.java
@@ -23,6 +23,8 @@ public class Player {
 	
 	        Direction[] directions = Direction.values();
 	        
+	        
+	        // These will help keep track of robots and the such
 	        TreeMap<Integer, Location> unitLocations = new TreeMap<Integer, Location>();
 	        ArrayList<Integer> ID = new ArrayList<Integer>();
 	        TreeMap<Integer, Integer> activeFactoryBuilds = new TreeMap<Integer, Integer>();
@@ -32,12 +34,15 @@ public class Player {
 	           System.out.println("Current round: "+gc.round());
 	           System.out.println("Karbonite " + gc.karbonite());
 	      
+	           // Every round the units and location will be updated
 	           for (int i = 0; i < gc.myUnits().size(); i++)
 	        	   {
 	        		unitLocations.put(gc.myUnits().get(i).id() ,gc.myUnits().get(i).location());
 	        		ID.add(gc.myUnits().get(i).id());
 	        	   }
 	           
+	           
+	           // Only occurs in the first round
 	           if (gc.round() == 1 && gc.planet() == Planet.Earth) {
 	        	    blueprintFactory(0, ID, gc);
 	        	    long size = gc.myUnits().size() - 1;
@@ -47,7 +52,10 @@ public class Player {
 	           gc.nextTurn();
 	        }
 	 
-	 }           
+	 }  
+	 
+	 // This method will check and see if a specified worker can set a factory blueprint around him/her
+	 
 	      public static void blueprintFactory (int i, ArrayList<Integer> ID, GameController gc)
 	      {
 	    	  if (gc.canBlueprint(ID.get(i), UnitType.Factory, Direction.North) == true)
@@ -68,11 +76,14 @@ public class Player {
 				   	gc.blueprint(ID.get(i), UnitType.Factory, Direction.West);
 	      }
 	      
+	      // This method will work with the activeFactoryBuilds tree map to allow workers to build on a blueprint
 	      public static void buildFactory(int worker, int factory, GameController gc) {
 	    	  
 	    	  gc.build(worker, factory);
 	      }
 	      
+	      
+	      // Might not need this....update coming
 	      public static int getFactoryID(ArrayList<Integer> ID, GameController gc)
 	      {
 	    	  for (int i = 0; i < gc.myUnits().size(); i++) {
