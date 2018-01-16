a0461f91347f306a6b8e89a71904ac290ff3f100
kushtimusPrime <30348781+kushtimusPrime@users.noreply.github.com>
2018-01-16 12:07:11 -0500

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,4 +1,4 @@
-// Nathan Hellstedt, Kysh Hari, Charlie Little
+// Nathan Hellstedt, Kush Hari, Charlie Little, Gordon MacDonald
 
 import bc.Direction;
 import bc.GameController;
@@ -226,6 +226,33 @@ public class Player {
   		  }
 	    	  }
 	      }
+		 //This method will determine which round the rocket should launch in order to reach Mars the earliest 
+	 public static long getFirstLaunchRound(GameController gc) {
+		 //currentPattern is the orbit pattern of the map
+		 OrbitPattern currentPattern=gc.orbitPattern();
+		 //marsArrivals is an arrayList that records when a rocket would arrive on Mars based on which round it launched
+		 //marsArrivals keeps track of rounds 125-750 launches
+		 ArrayList<Long> marsArrivals=new ArrayList<Long>();
+		 //Puts the arrival round number into the ArrayList
+		 for(int a=125;a<=750;a++) {
+			 long duration=currentPattern.duration(a);
+			 long arrival=duration+a;
+			 marsArrivals.add(arrival);
+		 }
+		 //min is the lowest Mars arrival time
+		 long min=marsArrivals.get(0);
+		 //leaveTime is a round number that determines when a rocket should leave Earth in order to get to Mars the quickest
+		 int leaveTime=125;
+		 //The loop determines the min value and the leaveTime value
+		 for(int c=1;c<marsArrivals.size();c++) {
+			 long potentialLaunch=marsArrivals.get(c);
+			 if(potentialLaunch<min) {
+				 min=potentialLaunch;
+				 leaveTime=c+125;
+			 }
+		 }
+		 return leaveTime;
+	 }
 	      
 	      }
 
